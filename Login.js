af73836e80f8d94506f576f97c4672cbdbe30501
RyanOnkoba <ryan.onkoba@strathmore.edu>
2024-06-13 09:40:38 +0300

    Ryan

--- Login.js
+++ Login.js
@@ -1,1 +1,17 @@
-let user = "John", age = 25, message = "Ryan" ;
+// login.js
+
+function openModal(type) {
+    document.getElementById('loginModal').style.display = 'block';
+    document.getElementById('loginForm').action = '/' + type + '/login';
+}
+
+function closeModal() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == document.getElementById('loginModal')) {
+        closeModal();
+    }
+}
