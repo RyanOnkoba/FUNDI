9989d64d2613c3ada0a37cbf067cd2bb5e442486
RURIGI <rurigi.kariuki@strathmore.edu>
2024-07-18 14:47:46 +0300

    RURIGI

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import './App.css'
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
